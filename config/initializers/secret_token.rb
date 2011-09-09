b985a3bd5b6bda669f158f663ea26a4cc661df50
Jefferson Carley <jeff.carley@gmail.com>
2011-09-09 17:52:01 -0500

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shelveit::Application.config.secret_token = 'ac744a6730ec55b87a7edd559e8adf3056eebb1e4cd93cc40171cd4fe16771de299c7dacca1d4b30fa256f86b89159208de9baecdb35b46e8663f6e9495988fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Shelveit::Application.config.session_store :cookie_store, :key => '_shelveit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Shelveit::Application.config.session_store :active_record_store
